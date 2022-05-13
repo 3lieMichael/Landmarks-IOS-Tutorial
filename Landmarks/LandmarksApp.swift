f130fb99dae643f6d32b55a96c375a9ab9e087df
elie-michael <ngandu.em@gmail.com>
2022-05-13 21:02:10 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Elie Michael on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
