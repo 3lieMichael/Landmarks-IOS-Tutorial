9b481b1ce266949bd809d1176b73fd64edfa3cd3
elie-michael <ngandu.em@gmail.com>
2022-05-15 02:37:13 +0200

    Init

---
+++ Landmarks/Views/Helpers/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Elie Michael on 2022/05/13.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 3)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
